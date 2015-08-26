995d42ed58f216662c2cc13e26fffab458654fbb
giner7415 <giner7415@gmail.com>
2015-08-25 20:21:12 -0500

    Modificando el CSS mediante JavaScript

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # calculadoraJavaScript
 Calculadora básica en JavaScript
 
-Deben estar almacenados los tres archivos en una misma carpeta o cambiar las rutas a placer en el index y despues ejecutarlo para visualizar la culculadora.
-El archivo style contiene todo lo corrspondiente al diseño y el archivo script posee la funcionalidad, para lo cual muestra el resultado de las operaciones mediante el metodo eval().
+En este apartado se agrego dos botones correspondientes pra marcar los numeros y los operadores respectivamente. Ademas otros dos botones para voler la calculadora al estado inicial.
--- script.js
+++ script.js
@@ -16,3 +16,48 @@ function pulsada (tecla) {
                 break;
         }
 }
+ 
+function marcarOperadores() {
+        var listaNodosOperadores = document.querySelectorAll("#calculadora .teclas span.operador");
+        var nodoIgual = document.querySelector (".igual");
+        for (var i=0; i<listaNodosOperadores.length; i++) {
+                listaNodosOperadores[i].style.backgroundColor = "yellow";
+                listaNodosOperadores[i].style.color = "black";
+                listaNodosOperadores[i].style.border = "solid 1px";
+                }
+        nodoIgual.style.backgroundColor = "yellow";
+        nodoIgual.style.color = "black";
+        nodoIgual.style.border = "solid 1px";
+}
+
+function desmarcarOperadores(){
+        var listaNodosOperadores = document.querySelectorAll("#calculadora .teclas span.operador");
+        var nodoIgual = document.querySelector (".igual");
+        for (var i=0; i<listaNodosOperadores.length; i++) {
+                listaNodosOperadores[i].style.backgroundColor = "#FFDAB9";
+                listaNodosOperadores[i].style.color = " #888888";
+                listaNodosOperadores[i].style.border = "solid 0px";
+                }
+        nodoIgual.style.backgroundColor = "#F1FF92";
+        nodoIgual.style.color = " #888888";
+        nodoIgual.style.border = "solid 0px";
+}
+ 
+function marcarNumeros() {
+        var listaNodosNumeros = document.querySelectorAll("#calculadora .teclas span.numero");
+        for (var i=0; i<listaNodosNumeros.length; i++) {
+                listaNodosNumeros[i].style.backgroundColor = "black";
+                listaNodosNumeros[i].style.color = "white";
+                listaNodosNumeros[i].style.border = "solid 1px";
+                }
+}
+
+function desmarcarNumeros(){
+        var listaNodosNumeros = document.querySelectorAll("#calculadora .teclas span.numero");
+        for (var i=0; i<listaNodosNumeros.length; i++) {
+                listaNodosNumeros[i].style.backgroundColor ="#FFFFFF";
+                listaNodosNumeros[i].style.color = "#888888";
+                listaNodosNumeros[i].style.border = "solid 0px";
+                }
+}
+
